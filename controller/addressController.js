6574bb29f878558a8a1cb4f18141d0e89bc48bfb
Bhaskar Borah <borahb563@gmail.com>
2023-03-04 00:49:58 +0530

    added user address route

---
+++ controller/addressController.js
@@ -0,0 +1,66 @@
+const UserAddress = require("../models/addressModel")
+const asynchandler = require("express-async-handler")
+const async = require("async")
+
+
+//@desc add address
+//@api POST hserver/user/address
+//acess private
+
+const addAddress = asynchandler(async(req,res)=>{
+const { payload } = req.body;
+  if (payload.address) {
+    if (payload.address._id) {
+      UserAddress.findOneAndUpdate(
+        { user_id: req.user.user.id, "address._id": payload.address._id },
+        {
+          $set: {
+            "address.$": payload.address,
+          },
+        }
+      ).exec((error, address) => {
+        if (error) return res.status(400).json({ error });
+        if (address) {
+          res.status(201).json({ address });
+        }
+      });
+    } else {
+      UserAddress.findOneAndUpdate(
+        { user_id: req.user.user.id },
+        {
+          $push: {
+            address: payload.address,
+          },
+        },
+        { new: true, upsert: true }
+      ).exec((error, address) => {
+        if (error) return res.status(400).json({ error });
+        if (address) {
+          res.status(201).json({ address });
+        }
+      });
+    }
+  } else {
+    res.status(400).json({ error: "Params address required" });
+  }
+
+    
+})
+
+
+//@desc get address
+//@api GET hserver/user/address
+//acess private
+
+const getAddress = asynchandler(async(req,res)=>{
+    UserAddress.findOne({user_id: req.user.user.id})
+    .then((response)=>{
+        res.json({response})
+    }).catch((err)=>{
+        throw new Error(err)
+    })
+    
+        
+    })
+
+module.exports = {addAddress,getAddress}
